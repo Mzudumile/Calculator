bf5141a3270686205b5425bb9c9a91758a2baf89
mzudumile <mzudumile.kaizer@gmail.com>
2025-12-01 16:26:06 +0200

    basic operations

--- calculator.js
+++ calculator.js
@@ -1,40 +1,125 @@
-var screenEle = document.getElementById("expression");
-var express = [];
-var tempnum = "";
-function addnumber(num) {
-    if (screenEle) {
-        screenEle.value += num;
-        tempnum = tempnum.concat(num);
-    }
-}
-function addToArray(opperation) {
-    express.push(tempnum);
-    console.log("adding");
-    tempnum = "";
-    screenEle.value += opperation;
-}
-// Add function
-function add() {
-}
-// subtract function
-function subtract() {
-}
-// multiply function
-function multiply() {
-}
-function divide() {
+var screen = document.getElementById("expression");
+function append(value) {
+    screen.value += value;
 }
 function clearScreen() {
-    screenEle.value = "";
+    screen.value = "";
 }
-function Answer() {
-    //let expr: String = screenEle.value;
-    var numBefore;
-    var numAfter;
-    if (express.indexOf('+')) {
-        numBefore = parseInt(express[express.indexOf('+') - 1]);
-        numAfter = parseInt(express[express.indexOf('+') + 1]);
-        var answer = numBefore + numAfter;
-        screenEle.value = answer.toString();
+/* ------------------------------
+   TOKENIZER
+--------------------------------*/
+function tokenize(expr) {
+    var tokens = [];
+    var number = "";
+    for (var i = 0; i < expr.length; i++) {
+        var c = expr[i];
+        if (/\d|\./.test(c)) {
+            number += c;
+        }
+        else {
+            if (number.length > 0) {
+                tokens.push(number);
+                number = "";
+            }
+            if ("+-*/()√".includes(c)) {
+                tokens.push(c);
+            }
+        }
+    }
+    if (number.length > 0)
+        tokens.push(number);
+    return tokens;
+}
+/* ------------------------------
+   SHUNTING-YARD (to RPN)
+--------------------------------*/
+var precedence = {
+    "√": 4,
+    "*": 3,
+    "/": 3,
+    "+": 2,
+    "-": 2
+};
+function toRPN(tokens) {
+    var output = [];
+    var stack = [];
+    for (var _i = 0, tokens_1 = tokens; _i < tokens_1.length; _i++) {
+        var token = tokens_1[_i];
+        if (!isNaN(Number(token))) {
+            output.push(token);
+        }
+        else if (token === "√") {
+            stack.push(token);
+        }
+        else if ("+-*/".includes(token)) {
+            while (stack.length > 0 &&
+                precedence[stack[stack.length - 1]] >= precedence[token]) {
+                output.push(stack.pop());
+            }
+            stack.push(token);
+        }
+        else if (token === "(") {
+            stack.push(token);
+        }
+        else if (token === ")") {
+            while (stack.length && stack[stack.length - 1] !== "(") {
+                output.push(stack.pop());
+            }
+            stack.pop(); // remove "("
+        }
+    }
+    while (stack.length) {
+        output.push(stack.pop());
+    }
+    return output;
+}
+/* ------------------------------
+   RPN EVALUATOR
+--------------------------------*/
+function evalRPN(rpn) {
+    var stack = [];
+    for (var _i = 0, rpn_1 = rpn; _i < rpn_1.length; _i++) {
+        var token = rpn_1[_i];
+        if (!isNaN(Number(token))) {
+            stack.push(Number(token));
+            continue;
+        }
+        if (token === "√") {
+            var a = stack.pop();
+            stack.push(Math.sqrt(a));
+        }
+        else {
+            var b = stack.pop();
+            var a = stack.pop();
+            switch (token) {
+                case "+":
+                    stack.push(a + b);
+                    break;
+                case "-":
+                    stack.push(a - b);
+                    break;
+                case "*":
+                    stack.push(a * b);
+                    break;
+                case "/":
+                    stack.push(a / b);
+                    break;
+            }
+        }
+    }
+    return stack[0];
+}
+/* ------------------------------
+   MAIN CALCULATE FUNCTION
+--------------------------------*/
+function calculate() {
+    try {
+        var tokens = tokenize(screen.value);
+        var rpn = toRPN(tokens);
+        var answer = evalRPN(rpn);
+        screen.value = answer.toString();
+    }
+    catch (_a) {
+        screen.value = "Error";
     }
 }
--- calculator.ts
+++ calculator.ts
@@ -1,59 +1,127 @@
-var screenEle:HTMLElement|null  = document.getElementById("expression");
-var express: any = []
-var tempnum: string = "";
+let screen = document.getElementById("expression") as HTMLInputElement;
 
-function addnumber(num: string): void {
-    if (screenEle){
-        screenEle.value += num;
-        tempnum = tempnum.concat(num);
+function append(value: string): void {
+    screen.value += value;
+}
+
+function clearScreen(): void {
+    screen.value = "";
+}
+
+/* ------------------------------
+   TOKENIZER
+--------------------------------*/
+function tokenize(expr: string): string[] {
+    let tokens: string[] = [];
+    let number = "";
+
+    for (let i = 0; i < expr.length; i++) {
+        let c = expr[i];
+
+        if (/\d|\./.test(c)) {
+            number += c;
+        } else {
+            if (number.length > 0) {
+                tokens.push(number);
+                number = "";
+            }
+            if ("+-*/()√".includes(c)) {
+                tokens.push(c);
+            }
+        }
+    }
+    if (number.length > 0) tokens.push(number);
+
+    return tokens;
+}
+
+/* ------------------------------
+   SHUNTING-YARD (to RPN)
+--------------------------------*/
+
+const precedence: any = {
+    "√": 4,
+    "*": 3,
+    "/": 3,
+    "+": 2,
+    "-": 2
+};
+
+function toRPN(tokens: string[]): string[] {
+    let output: string[] = [];
+    let stack: string[] = [];
+
+    for (let token of tokens) {
+        if (!isNaN(Number(token))) {
+            output.push(token);
+        } else if (token === "√") {
+            stack.push(token);
+        } else if ("+-*/".includes(token)) {
+            while (
+                stack.length > 0 &&
+                precedence[stack[stack.length - 1]] >= precedence[token]
+            ) {
+                output.push(stack.pop()!);
+            }
+            stack.push(token);
+        } else if (token === "(") {
+            stack.push(token);
+        } else if (token === ")") {
+            while (stack.length && stack[stack.length - 1] !== "(") {
+                output.push(stack.pop()!);
+            }
+            stack.pop(); // remove "("
+        }
+    }
+
+    while (stack.length) {
+        output.push(stack.pop()!);
+    }
+
+    return output;
+}
+
+/* ------------------------------
+   RPN EVALUATOR
+--------------------------------*/
+function evalRPN(rpn: string[]): number {
+    let stack: number[] = [];
+
+    for (let token of rpn) {
+        if (!isNaN(Number(token))) {
+            stack.push(Number(token));
+            continue;
+        }
+
+        if (token === "√") {
+            const a = stack.pop()!;
+            stack.push(Math.sqrt(a));
+        } else {
+            const b = stack.pop()!;
+            const a = stack.pop()!;
+
+            switch (token) {
+                case "+": stack.push(a + b); break;
+                case "-": stack.push(a - b); break;
+                case "*": stack.push(a * b); break;
+                case "/": stack.push(a / b); break;
+            }
+        }
+    }
+
+    return stack[0];
+}
+
+/* ------------------------------
+   MAIN CALCULATE FUNCTION
+--------------------------------*/
+function calculate(): void {
+    try {
+        const tokens = tokenize(screen.value);
+        const rpn = toRPN(tokens);
+        const answer = evalRPN(rpn);
+        screen.value = answer.toString();
+    } catch {
+        screen.value = "Error";
     }
 }
-
-function addToArray(opperation: string): void{
-    express.push(tempnum);
-    console.log("adding")
-    tempnum = "";
-    screenEle.value += opperation;
-}
-
-// Add function
-
-function add(): void{
-
-}
-
-// subtract function
-
-function subtract(): void{
-
-}
-
-// multiply function
-
-function multiply(): void{
-
-}
-
-function divide(): void{
-
-}
-
-function clearScreen (){
-    screenEle.value = "";
-}
-
-function Answer(): void{
-
-    //let expr: String = screenEle.value;
-    let numBefore:number;
-    let numAfter:number;
-
-    if (express.indexOf('+')){
-        numBefore = parseInt(express[express.indexOf('+') - 1]);
-        numAfter = parseInt(express[express.indexOf('+') + 1]);
-        let answer:number =  numBefore + numAfter;
-        screenEle.value = answer.toString();
-    }
-}
-
-
